ca0f4ab7dfa12dbba62e5d3b5d6d7911200bbdab
pshpark7 <pshpark07@gmail.com>
2024-04-16 17:25:40 +0900

    프로젝트 세팅

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'teamProject_2024_04',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
